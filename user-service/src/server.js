c23b11e8be74c6977a2c8a99be629963513ac000
Rumaiz M.R.M <rumaizfourdiv@gmail.com>
2026-02-03 10:37:19 +0530

    user service package issue fix

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const amqp = require('amqplib');
 const winston = require('winston');
